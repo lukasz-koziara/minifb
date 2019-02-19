ebf389f272c035651d82e6b48f64a114ea0e1f23
lukasz_koziara1988 <lukasz.koziara88@gmail.com>
2019-02-19 14:15:46 +0100

    added User Schema Model

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -7,23 +7,22 @@ const posts = require('./routes/api/posts');
 
 const app = express();
 
-//DB Config
+// DB Config
 const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-    .connect(db)
-    .then(() => console.log('MongoDB Connected'))
-    .catch(err => console.log(err))
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
-app.get('/', (req, res) => res.send('Hello'));
+app.get('/', (req, res) => res.send('Hello World'));
 
 // Use Routes
 app.use('/api/users', users);
 app.use('/api/profile', profile);
-app.use('/api/posts', posts );
+app.use('/api/posts', posts);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server running on port ${port}`))
-
+app.listen(port, () => console.log(`Server running on port ${port}`));
