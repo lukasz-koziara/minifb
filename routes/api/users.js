58bc9f2fdd5dcc35ecd18e9553aa31dcdd5635ab
Koza88 <lukasz.koziara88@gmail.com>
2019-04-11 11:28:58 +0200

    added create and update profile

--- routes/api/users.js
+++ routes/api/users.js
@@ -11,7 +11,7 @@ const validateRegisterInput = require('../../validation/register');
 const validateLoginInput = require('../../validation/login');
 
 // Load User model
-const User = require('../../models/Users');
+const User = require('../../models/User');
 
 // @route   GET api/users/test
 // @desc    Tests users route
@@ -29,7 +29,8 @@ router.post('/register', (req, res) => {
     return res.status(400).json(errors);
   }
 
-  User.findOne({ email: req.body.email }).then(user => {
+  User.findOne({ email: req.body.email })
+  .then(user => {
     if (user) {
       errors.email = 'Email already exists';
       return res.status(400).json(errors);
